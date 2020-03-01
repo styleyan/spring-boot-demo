5676d5090735aadd33f50d042569aec1a96beb8f
yanxiaofei <forget_love2161@126.com>
2020-03-01 22:14:08 +0800

    feat: 增加mongodb测试文件

---
+++ mongodb/src/main/java/com/isyxf/mongodb/controller/BaseController.java
@@ -0,0 +1,43 @@
+package com.isyxf.mongodb.controller;
+
+import com.isyxf.mongodb.empty.Book;
+import com.isyxf.mongodb.service.MongoDbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author yanxiaofei
+ */
+@RestController
+public class BaseController {
+
+    @Autowired
+    private MongoDbService mongoDbService;
+
+    @PostMapping("/mongo/save")
+    public String saveObj(@RequestBody Book book) {
+        return mongoDbService.saveAobj(book);
+    }
+
+    @GetMapping("/mongo/findAll")
+    public List<Book> findAll() {
+        return mongoDbService.findAll();
+    }
+
+    @GetMapping("/mongo/findOne")
+    public Book findOne(@RequestParam String id) {
+        return mongoDbService.getBookById(id);
+    }
+
+    @PostMapping("/mongo/update")
+    public String update(@RequestBody Book book) {
+        return mongoDbService.updateBook(book);
+    }
+
+    @GetMapping("/mongo/delById")
+    public String delById(@RequestParam String id) {
+        return mongoDbService.deleteBookById(id);
+    }
+}
---
+++ mongodb/src/main/java/com/isyxf/mongodb/empty/Book.java
@@ -0,0 +1,48 @@
+package com.isyxf.mongodb.empty;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Description: 图书实体类
+ */
+@Data
+@Document(collection = "yxf_book")
+public class Book {
+    @Id
+    private String id;
+    /**
+     * 价格
+     */
+    private Integer price;
+    /**
+     * 书名
+     */
+    private String name;
+    /**
+     * 简介
+     */
+    private String info;
+    /**
+     * 出版社
+     */
+    private String publish;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 列表
+     */
+    private List<String> authors = new ArrayList<>();
+}
