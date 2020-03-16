e92f904c4ba3560fe9597b13069cf6427a1d226a
yanxiaofei <forget_love2161@126.com>
2020-03-16 23:58:30 +0800

    feat: 异常处理

--- missError/src/main/java/com/isyxf/error/controller/Mytest.java
+++ missError/src/main/java/com/isyxf/error/controller/Mytest.java
@@ -1,5 +1,6 @@
 package com.isyxf.error.controller;
 
+import com.isyxf.error.exception.http.ForbiddenException;
 import com.isyxf.error.exception.http.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,15 @@ public class Mytest {
     @GetMapping("/api/two")
     public String two() {
         throw new RuntimeException("这里错误了");
+    }
+
+    @GetMapping("/api/three")
+    public String three() throws Exception {
+        throw new Exception("这里错误了");
+    }
+
+    @GetMapping("/api/four")
+    public String four() {
+        throw new ForbiddenException(233);
     }
 }
--- missError/src/main/java/com/isyxf/error/exception/GlobalExceptionAdvice.java
+++ missError/src/main/java/com/isyxf/error/exception/GlobalExceptionAdvice.java
@@ -1,10 +1,13 @@
-package com.isyxf.error.core;
+package com.isyxf.error.exception;
+import com.isyxf.error.core.UnifyResponse;
+import com.isyxf.error.exception.http.ForbiddenException;
 import com.isyxf.error.exception.http.HttpException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLSyntaxErrorException;
 
 /**
  * 统一管理错误
@@ -23,6 +26,29 @@ public class GlobalExceptionAdvice {
         System.out.println("hell word");
         UnifyResponse unifyResponse = new UnifyResponse(9999, "服务器异常", "233/2323");
         return unifyResponse;
+    }
+
+    /**
+     * 运行时异常
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = RuntimeException.class)
+    @ResponseBody
+    public UnifyResponse handleRuntimeException(HttpServletRequest req, RuntimeException e) {
+        System.out.println(e.getMessage());
+        return null;
+    }
+
+    /**
+     * 指定异常类型
+     */
+    @ExceptionHandler(value = ForbiddenException.class)
+    @ResponseBody
+    public UnifyResponse handleForbiddenException(HttpServletRequest req, ForbiddenException e) {
+        System.out.println(e.getMessage());
+        return  null;
     }
 
     /**
